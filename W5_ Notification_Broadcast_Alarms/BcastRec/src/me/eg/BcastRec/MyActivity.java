f2eeeab99b57059c85d645910d553c31d74f39c7
suiyueyu <suiyueyu@gmail.com>
2015-01-06 00:28:45 +0800

    练习2-1 广播与接收

--- W5_ Notification_Broadcast_Alarms/BcastRec/src/me/eg/BcastRec/MyActivity.java
+++ W5_ Notification_Broadcast_Alarms/BcastRec/src/me/eg/BcastRec/MyActivity.java
@@ -1,9 +1,21 @@
 package me.eg.BcastRec;
 
+
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyActivity extends Activity {
+
+    private static final String CUSTOM_INTENT = "me.eg.BcastRec.show_toast";
+    private final Receiver1 receiver1 = new Receiver1();
+    private final IntentFilter intentFilter = new IntentFilter(CUSTOM_INTENT);
+
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +23,24 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        registerReceiver(receiver1,intentFilter);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendBroadcast(new Intent(CUSTOM_INTENT),
+                        Manifest.permission.VIBRATE);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(receiver1);
+        super.onDestroy();
+
     }
 }
